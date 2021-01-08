3f2916ab0f54854424a776bf4d116b61294e4af7
Yana <yanas@bluewin.ch>
2021-01-08 21:16:32 +0100

    Configure Babel with webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-};
+module.exports = (api) => ({
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "entry",
+          // caller.target will be the same as the target option from webpack
+          targets: api.caller((caller) => caller && caller.target === "node")
+            ? { node: "current" }
+            : { chrome: "58", ie: "11" },
+        },
+      ],
+    ],
+  });
