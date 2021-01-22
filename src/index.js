dd3c8964858ec22be2f72e22b3efef4579174e7e
Yana <yanas@bluewin.ch>
2021-01-22 16:51:58 +0100

    Start to create a web

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import Post from "./Post";
-// import json from'./assets/json.json'
-// import xml from './assets/data.xml'
-// import csv from './assets/data.csv'
-import WebpackLogo from "./assets/webpack-logo.png";
-// import './babel.js'
+// import WebpackLogo from "./assets/webpack-logo.png";
 import "./styles/styles.css";
 
-const post = new Post("Webpack Post Title", WebpackLogo);
+// eslint-disable-next-line func-names
+(async function () {
+  // Получаем указатели на нужные элементы
+  const formEl = document.querySelector("form");
+  const weatherInfoEl = document.querySelector("#weatherInfo");
 
-console.log("Post to string:", post.toString());
+  function showWeather(el, weatherInfo) {
+    // eslint-disable-next-line no-param-reassign
+    el.innerHTML = JSON.stringify(weatherInfo, null, 2);
+  }
 
-// $('pre').addClass('code').html(post.toString())
+  async function getWeather(cityName) {
+    const weather = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?units=${cityName}&appid=8eda416857ac90170ef15cebf17e8fd1`
+    );
+    return (await weather).json();
+  }
 
-// console.log('JSON:', json)
-// console.log('XML:', xml)
-// console.log('CSV:', csv)
+  formEl.addEventListener("submit", async (ev) => {
+    // чтобы не перезагружать страницу
+    ev.preventDefault();
+
+    // читаем значение из формы
+    const formElement = ev.target;
+    const inputEl = formElement.querySelector("input");
+    const cityName = inputEl.value;
+    inputEl.value = "";
+
+    const weather = await getWeather(cityName);
+    showWeather(weatherInfoEl, weather);
+  });
+})();
