c5fe59722dbbf56c4ed824b1c918383a17cd4952
Yana <yanas@bluewin.ch>
2021-01-23 18:57:04 +0100

    Resolve problem with promise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import WebpackLogo from "./assets/webpack-logo.png";
 import "./styles/styles.css";
 
 // eslint-disable-next-line func-names
@@ -12,11 +11,25 @@ import "./styles/styles.css";
     el.innerHTML = JSON.stringify(weatherInfo, null, 2);
   }
 
+  async function getUserLocation() {
+    const url = `https://get.geojs.io/v1/ip/geo.json`;
+    const response = await fetch(url);
+    const json = await response.json();
+    return json.city;
+  }
+
   async function getWeather(cityName) {
     const weather = fetch(
       `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=8eda416857ac90170ef15cebf17e8fd1`
     );
     return (await weather).json();
+  }
+
+  async function getUserMap(cityName) {
+    const url = `https://maps.googleapis.com/maps/api/staticmap?center=${cityName}=14&size=400x400&key=AIzaSyDANLeEnr2Wf05hG0wxHA0Ucqz5CeZF_Cw`;
+    const response = await fetch(url);
+    const map = await response.text();
+    return map;
   }
 
   formEl.addEventListener("submit", async (ev) => {
@@ -32,4 +45,8 @@ import "./styles/styles.css";
     const weather = await getWeather(cityName);
     showWeather(weatherInfoEl, weather);
   });
+  const userCity = await getUserLocation();
+  const userWeather = await getWeather(userCity);
+  showWeather(weatherInfoEl, userWeather);
+  getUserMap();
 })();
